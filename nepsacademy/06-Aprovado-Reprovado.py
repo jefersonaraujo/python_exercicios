9d069210232a7533d6b9fdb9497276933fc13bfd
jeferson <jeferson@master.jeferson.local>
2020-02-01 15:11:32 -0300

    exercicios

---
+++ nepsacademy/06-Aprovado-Reprovado.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python
+# coding : utf-8
+
+
+#Recebe duas variaveis na mesma linha 5 6 
+A,B = input().split()
+ 
+#converter para o tipo int
+A = float(A)
+B = float(B)
+
+#Processamento
+
+media = (A + B)/2
+resposta =''
+
+if media >= 7:
+    resposta  ='Aprovado'
+elif media <7 and media >=4:
+    resposta  ='Recuperacao'
+else:
+    resposta  ='Reprovado'
+
+    
+    
+print(resposta)
